61530c9d7cbd3b4a88a2b183c895870cc058cf95
zo-el <joelulahanna@gmail.com>
2025-02-14 23:18:33 -0600

    update slogan

--- src/App.vue
+++ src/App.vue
@@ -13,29 +13,24 @@
 
         <p
           class="text-white text-xl sm:text-xl lg:text-2xl max-w-xl sm:max-w-2xl lg:max-w-4xl xl:max-w-5xl font-light leading-relaxed">
-          Mutual credit accounting systems for DePIN that are immutable, transparent, and backed by productive capacity.
+          Unlock the Potential of DePIN with Microtransaction Accounting
         </p>
 
         <div class="mt-8 sm:mt-16 lg:mt-20">
           <p class="text-white text-2xl sm:text-2xl lg:text-3xl font-normal">
             Interested in testing?
-            <a href="mailto:info@unyt.co" class="metallic-text hover:opacity-80 transition-opacity duration-300 ml-2"
-              >info@unyt.co</a
-            >
+            <a href="mailto:info@unyt.co"
+              class="metallic-text hover:opacity-80 transition-opacity duration-300 ml-2">info@unyt.co</a>
           </p>
         </div>
       </div>
 
       <div class="absolute top-4 right-4 sm:top-6 sm:right-6 lg:top-8 lg:right-8">
-        <a
-          href="https://github.com/unytco/hfvz-releases/releases"
-          target="_blank"
-          rel="noopener noreferrer"
+        <a href="https://github.com/unytco/hfvz-releases/releases" target="_blank" rel="noopener noreferrer"
           class="text-xl sm:text-xl transition-colors duration-300 group">
           <span class="metallic-text">Releases</span>
-          <span class="inline-block ml-1 opacity-0 group-hover:opacity-100 transition-opacity duration-300 metallic-text"
-            >↗</span
-          >
+          <span
+            class="inline-block ml-1 opacity-0 group-hover:opacity-100 transition-opacity duration-300 metallic-text">↗</span>
         </a>
       </div>
     </main>
@@ -43,11 +38,10 @@
     <footer class="w-full relative mt-auto pb-8 sm:pb-0">
       <div
         class="container mx-auto px-4 sm:px-8 lg:px-16 py-4 sm:py-8 lg:py-12 max-w-[1920px] flex flex-col items-center sm:block">
-        <img
-          src="@/assets/PBH_logo_white.svg"
-          alt="Powered by Holochain"
+        <img src="@/assets/PBH_logo_white.svg" alt="Powered by Holochain"
           class="h-16 sm:h-14 md:h-20 lg:h-24 xl:h-32 w-auto" />
-        <p class="text-white/40 text-sm font-light mt-2 sm:mt-0 sm:absolute sm:bottom-6 sm:right-8 lg:bottom-12 lg:right-16">
+        <p
+          class="text-white/40 text-sm font-light mt-2 sm:mt-0 sm:absolute sm:bottom-6 sm:right-8 lg:bottom-12 lg:right-16">
           © {{ new Date().getFullYear() }} Unyt.co All rights reserved.
         </p>
       </div>
