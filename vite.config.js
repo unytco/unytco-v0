75a64822cf98b893885c4a9a17d70b18a28da2ee
zo-el <joelulahanna@gmail.com>
2025-02-17 16:25:29 -0600

    host at root

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,6 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import path from "path";
 
-// Get the branch name from environment variable
-const branch = process.env.GITHUB_REF_NAME || "main";
-
 export default defineConfig({
   plugins: [vue()],
   resolve: {
@@ -12,6 +9,5 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  // Set base depending on branch
-  base: branch === "develop" ? "/unyt.co/" : "/",
+  base: "/",
 });
